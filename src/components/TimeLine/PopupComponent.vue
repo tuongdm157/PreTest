40f1042a5d07e314b9bebbb841609cc43e562997
LDAP\dmtuong <dmtuong@cmc.com.vn>
2019-12-04 17:19:00 +0700

    Commit code

---
+++ src/components/TimeLine/PopupComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <b-modal id="my-modal" title="Coupon" centered size="lg" style="height: 800px">
+      <div class="text-left">
+        <span class="des">Pre-valid and valid coupons are listed</span>
+      </div>
+      <hr />
+      <div>
+        <b-row>
+          <b-col cols="2">
+            <img src="@/assets/thumbnail.png" alt width="80px" />
+          </b-col>
+          <b-col cols="7">
+            <span class="title-color">Valid</span>
+            <br />
+            <span>test</span>
+            <br />
+            <span class="sp-des">Validity period 02/12/2019 00:00 ~ 08/07/2018 23:59</span>
+          </b-col>
+          <b-col cols="3" class="btn-center">
+            <b-button variant="outline-success">Select</b-button>
+          </b-col>
+        </b-row>
+      </div>
+      <hr />
+      <div slot="modal-footer" class="w-100">
+        <div class="mt-2 btn-center">
+          <b-button class="not-focusable btn-fontSize">Close</b-button>
+        </div>
+      </div>
+    </b-modal>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style>
+.sp-des {
+  font-size: smaller;
+  font-weight: 500;
+  color: #ccc;
+}
+.des {
+  color: #ccc;
+}
+.title-color {
+  color: #007bff;
+}
+</style>
--- src/components/TimeLine/TimeLineComponent.vue
+++ src/components/TimeLine/TimeLineComponent.vue
@@ -5,11 +5,11 @@
       <hr />
 
       <div class="mt-5">
-        <b-button>Save draft</b-button>
-        <b-button variant="success" style="float: right">Save draft</b-button>
+        <b-button class="not-focusable btn-saveDraft btn-fontSize">Save draft</b-button>
+        <b-button variant="success" class="not-focusable btn-fontSize" style="float: right">Schedule</b-button>
         <hr />
       </div>
-      <!-- #28a745 -->
+
       <div>
         <b-row>
           <b-col cols="1"></b-col>
@@ -36,19 +36,68 @@
         </b-row>
         <hr />
       </div>
+
       <div>
-        <i class="fa fa-image"></i>
-        <i class="fa fa-caret-square-o-right"></i>
-        <i class="fa fa-smile-o"></i>
-        <i class="fa fa-link"></i>
-        <i class="fa fa-folder"></i>
-        <i class="fa fa-address-book-o"></i>
+        <b-row>
+          <b-col cols="10">
+            <b-card>
+              <div slot="header">
+                <b-button class="btn btn-light not-focusable mt-1">
+                  <i class="fa fa-image"></i>
+                </b-button>
+                <b-button class="btn btn-light not-focusable mt-1">
+                  <i class="fa fa-caret-square-o-right"></i>
+                </b-button>
+                <b-button class="btn btn-light not-focusable mt-1">
+                  <i class="fa fa-smile-o"></i>
+                </b-button>
+                <b-button class="btn btn-light not-focusable mt-1" @click="getCoupon">
+                  <i class="fa fa-folder"></i>
+                </b-button>
+                <b-button class="btn btn-light not-focusable mt-1">
+                  <i class="fa fa-link"></i>
+                </b-button>
+                <b-button class="btn btn-light not-focusable">
+                  <i class="fa fa-address-book-o"></i>
+                </b-button>
+              </div>
+              <b-card>
+                <b-card-text>
+                  Some quick example text to build on the
+                  <em>card title</em> and make up the bulk of the card's
+                  content.
+                </b-card-text>
+              </b-card>
+            </b-card>
+          </b-col>
+        </b-row>
       </div>
+
+      <div class="mt-5 btn-center">
+        <b-button
+          class="not-focusable btn-fontSize"
+          variant="success"
+          size="lg"
+          style="width: 160px;"
+        >Schedule</b-button>
+      </div>
+      <PopupComponent />
     </b-container>
   </div>
 </template>
 <script>
-export default {};
+import PopupComponent from "./PopupComponent.vue";
+export default {
+  components: {
+    PopupComponent
+  },
+
+  methods: {
+    getCoupon(){
+      this.$bvModal.show('my-modal');
+    }
+  }
+};
 </script>
 
 <style>
@@ -56,5 +105,24 @@ export default {};
   color: #fff;
   border-color: #28a745;
   background-color: #28a745;
+}
+.not-focusable:focus {
+  outline: none !important;
+  -webkit-box-shadow: none !important;
+  box-shadow: none !important;
+}
+
+.btn-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.btn.btn-saveDraft {
+  color: black;
+  background-color: #ccc;
+  border: none;
+}
+.btn.btn-fontSize {
+  font-weight: 600;
 }
 </style>
